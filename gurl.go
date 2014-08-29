b2b799122956eb62b97ceff64bd6ac3e48ab96a7
Joe Baker <joesbaker@gmail.com>
2014-08-29 17:16:57 +0100

    Adding gurl files for SPDY load testing

---
+++ gurl.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/jmckaskill/gospdy"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"runtime"
+	"time"
+)
+
+import _ "crypto/sha512"
+
+var VERBOSE = 0
+
+// Connect loop
+func connect(start, done chan bool, url string) {
+
+	for {
+		<-start
+
+		tr := &spdy.Transport{}
+		tr.FallbackClient = &http.Client{}
+		client := &http.Client{Transport: tr}
+
+		r, err := client.Get(url)
+
+		if err != nil {
+			log.Fatal(err)
+			done <- true
+			return
+		}
+
+		defer r.Body.Close()
+
+		// Print the body
+		if VERBOSE > 0 {
+
+			body, err := ioutil.ReadAll(r.Body)
+
+			if err != nil {
+				log.Fatal(err)
+				done <- true
+				return
+			}
+
+			fmt.Printf(string(body))
+		}
+
+		done <- true
+	}
+}
+
+// Goroutine to keep consuming <-done
+func requests_done(done, end chan bool, n, c int) {
+
+	start_time := time.Now().UnixNano()
+	last_now := start_time
+
+	for i := 0; i < n; i++ {
+		<-done
+		if i%c == 0 && i != 0 {
+			now := time.Now().UnixNano()
+			interval := (now - last_now) / 1e6
+			fmt.Printf("%d reqs done. + %d msecs\n", i, interval)
+			last_now = now
+		}
+	}
+
+	interval := (time.Now().UnixNano() - last_now) / 1e6
+	fmt.Printf("%d reqs done. + %d msecs\n", n, interval)
+
+	total_time := time.Now().UnixNano() - start_time
+	fmt.Printf("\ntotal time: %f secs\n", float64(total_time)/1e9)
+
+	end <- true
+}
+
+func main() {
+
+	url := flag.String("url", "", "url to connect to: 'https://www.google.com'")
+	n := flag.Int("n", 1, "total number of requests")
+	c := flag.Int("c", 1, "number of parallel requests")
+
+	verbose := flag.Int("v", 0, "verbosity")
+
+	flag.Parse()
+
+	VERBOSE = *verbose
+
+	// Enable debug in gospdy code
+	if VERBOSE > 2 {
+		spdy.Log = func(format string, args ...interface{}) {
+			fmt.Printf(format, args...)
+		}
+	}
+
+	start := make(chan bool, *n)
+	done := make(chan bool, *n)
+
+	end := make(chan bool, *n)
+
+	// just one request
+	if *n == 1 && *c == 1 {
+
+		if VERBOSE == 0 {
+			VERBOSE = 1
+		}
+
+		go connect(start, done, *url)
+		start <- true
+		<-done
+
+		return
+	}
+
+	// Multiple Requests
+
+	// Setting Max Procs to the Number of CPU Cores
+	fmt.Printf("Max procs %d\n", runtime.GOMAXPROCS(runtime.NumCPU()))
+	fmt.Printf("Max procs %d\n\n", runtime.GOMAXPROCS(0))
+
+	go requests_done(done, end, *n, *c)
+
+	for i := 0; i < *c; i++ {
+		go connect(start, done, *url)
+		// start some goroutines immediately
+		start <- true
+	}
+
+	for i := *c; i < *n; i++ {
+		// fill in the chan so everybody can work
+		start <- true
+	}
+
+	// wait for all the requests to be terminated
+	<-end
+}
